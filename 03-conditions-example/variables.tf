8db2e5e8d7594ffa3104a6c5f8dcbc34ffc3bbf3
Muddukrishna1505 <muddukrishna524@gmail.com>
2026-07-01 13:33:31 +0530

    Added 03-conditions-example

---
+++ 03-conditions-example/variables.tf
@@ -0,0 +1,64 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-0220d79f3f480ecf5"
+  description = "RHEL AMI"
+
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+    
+
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+  description = "The size of the EC2 instance to deploy"
+
+
+  validation {
+    condition     = contains(["t3.micro", "t2.micro", "t3.small"], var.instance_type)
+    error_message = "Instance type should be either t3.micro or t3.small"
+  }
+
+}
+
+variable "sg_name" {
+  default = "allow_ssh"
+}
+
+variable "port_ingress" {
+  type    = number
+  default = 22
+
+}
+
+variable "port_egress" {
+  type    = number
+  default = 0
+
+}
+
+variable "protocol" {
+  type    = string
+  default = "tcp"
+
+}
+
+variable "cidr" {
+  type    = list(any)
+  default = ["0.0.0.0/0"]
+}
+
+variable "ec2_tags" {
+  type = map(any)
+  default = {
+    Name         = "Instance-2"
+    Project_Name = "Roboshop"
+    Environment  = "Dev"
+  }
+
+
+}
